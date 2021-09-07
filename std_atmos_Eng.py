99f06f1eff9a9388133e53d038180871729cdac0
Jaden Thurgood <jthurgood@areai.com>
2021-09-07 16:21:16 -0400

    Added docstrings to the std_atmos functions

--- std_atmos_Eng.py
+++ std_atmos_Eng.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def interp(h):
-    #This function takes a height in ft and returns the geopotential height in ft
-    #the temperature (R), Pressure (lbf/ft^2), Density (slugs/ft^3) and Speed of Sound (ft/s)
+    """ This function takes a height in ft and returns the geopotential height in ft,
+    the temperature (R), Pressure (lbf/ft^2), Density (slugs/ft^3) and Speed of Sound (ft/s) """
     
     #import the English Standard Atmosphere table of data from the CSV file
     data = np.genfromtxt("Standard_Atmosphere_English_table.csv",dtype='float',delimiter=',',skip_header=1)
--- std_atmos_SI.py
+++ std_atmos_SI.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def interp(h):
-    #This function takes a height in meters and returns the geopotential height in meters
-    #the temperature (K), Pressure (N/m^2), Density (kg/m^3) and Speed of Sound (m/s)
+    """ This function takes a height in meters and returns the geopotential height in meters,
+    the temperature (K), Pressure (N/m^2), Density (kg/m^3) and Speed of Sound (m/s) """
    
     #import the SI Standard Atmosphere table of data from the CSV file
     data = np.genfromtxt("Standard_Atmosphere_SI_table.csv",dtype='float',delimiter=',',skip_header=1)
