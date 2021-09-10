b8e1da67a8bae148f32f8e6a35d890a5b0db6617
Jaden Thurgood <jthurgood@areai.com>
2021-09-10 16:51:38 -0400

    Update the steady level CL computation for user inputs. Still need to handle default units

--- AircraftPerformance.py
+++ AircraftPerformance.py
@@ -5,13 +5,20 @@ import warnings
 
 class Airplane():
     def __init__(self):
+        #Aircraft Geometry
         self.wingspan = 25  #ft
         self.c_bar = 5 #ft
-        self.T0 = 5000 #lbf
-        self.AR = self.wingspan/self.c_bar
-        self.e = 0.8 #Oswald efficiency
         self.Sw = self.wingspan*self.c_bar #wing area
+
+        #Aricraft Characteristics
+        self.T0 = 5000 #lbf
+        self.e = 0.8 #Oswald efficiency
+        self.weight = 800
+        
+        #Conditions
         self.units = 'English'
+        self.V_inf = 100
+        self.altitude = 5000
 
     def get_density(self,alt,**kwargs):
         """ This function accepts the altitude as the argument and returns the density in slugs/ft^3 for "English" and N/m^3 for "SI"
@@ -44,7 +51,7 @@ class Airplane():
         and average chord length as keyword arguments 'span' and 'c_bar'. If the user specifies the span and chord length in the function call it will overite the 
         values from the input file if one is given. """
 
-        if kwargs: #check if the user input any kwargs for the aspect ration calculation
+        if kwargs: #check if the user input any kwargs for the aspect ratio calculation
             if ('span' in kwargs) and ('c_bar' not in kwargs): #check if the user only input a span value
                AR = kwargs['span']/self.c_bar 
             elif ('c_bar' in kwargs) and ('span' not in kwargs): #check if the user only input the average chord
@@ -58,9 +65,51 @@ class Airplane():
             AR = self.wingspan/self.c_bar
         return AR
     
-    def calc_steady_level_CL(self,W,V_inf,Sw):
-        rho = self.get_density(5000)
-        CL = W/(0.5*rho*V_inf*V_inf*Sw)
+    def calc_steady_level_CL(self,**kwargs): #function uses the small thrust angle approximation from phillips "Mechanics of Flight" Eq (3.2.24)
+        """ This function calculates CL for steady level flight assuming small thrust angles. It can do this using the input file values or the user can
+        manually change the conditions using keyword arguments 'weight', 'V_inf' for freestream velocity, 'Sw' for wing area, and 'alt' for altitude.
+        Any kewword input values will override the input file values. """
+
+        #unpack the values from kwargs or from Class self
+        if kwargs: #check if the user input any kwargs for stead CL calculation
+            
+            if 'weight' in kwargs: #if the user assigned a weight in the function call, then use it. 
+                w = kwargs['weight']
+            else: #Otherwise, default to the input file value
+                w = self.weight
+
+            if 'V_inf' in kwargs: #if the user assigned a freestream velocity in the function call, then use it. 
+                V = kwargs['V_inf']
+            else: #Otherwise, default to the input file value
+                V = self.V_inf
+
+            if 'Sw' in kwargs: #if the user assigned a reference area in the function call, then use it. 
+                area_ref = kwargs['Sw']
+            else: #Otherwise, default to the input file value
+                area_ref = self.Sw
+            
+            if 'alt' in kwargs: #if the user assigned an altitude in the function call, then use it. 
+                h = kwargs['alt']
+            else: #Otherwise, default to the input file value
+                h = self.altitude
+            
+            #if the user inputs a kwarg that is not used without any kwargs that are used. Throw an error
+            if (any(key in kwargs for key in ('weight','V_inf','Sw','alt'))): 
+                None
+            else:
+                raise ValueError("The only keyword arguments used in calc_steady_level_CL are any combination of 'weight', 'V_inf', 'Sw', and 'alt'.")
+        
+        else: #just use the values from the input file
+            ######## We need to figure out how we are going to handle units here. Should the user have to pass them in? Or do we perform more abstraction with the get density or condition?
+            w = self.weight
+            V = self.V_inf
+            area_ref = self.Sw
+            h = self.altitude
+        
+        #Perform final calculations
+        rho = self.get_density(h) #get the density
+        CL = w/(0.5*rho*V*V*area_ref) #from Eq. (3.2.24)
+
         return CL
 
     def calc_CD_from_coeff(self,CD0,CD0_L,CL, e):
@@ -77,4 +126,4 @@ class Airplane():
         return Pwr_req
 
 test_airplane = Airplane()
-print(test_airplane.get_AR())
+print(test_airplane.calc_steady_level_CL(weight=700))
