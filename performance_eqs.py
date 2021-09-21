d68af80848573bdac013942ef2b06fa2c1d50f6a
Jaden Thurgood <jthurgood@areai.com>
2021-09-21 08:38:13 -0400

    finished updating existing docstrings to reflect the input arguments

--- performance_eqs.py
+++ performance_eqs.py
@@ -4,7 +4,14 @@ import numpy as np
 
 #L/D max
 def L2D_max(e,Ra,CD0,CD0_L):
-    #""" Computes the maximum lift-to-drag ratio given an oswald efficiency, aspect ratio, and the coefficiencts of drag with respect to lift. """
+    """ Computes the maximum lift-to-drag ratio.
+    
+    Args:
+        e (float): Oswald efficiency (0-1)
+        Ra (float): Aspect ratio of the main wing 
+        CD0 (float): Drag coefficient at zero lift
+        CD0_L (float): The linear coefficient in the parabolic relation for drag coefficient as a function of the lift coefficient.        
+    """
 
     #Equation 3.2.13 from Warren Phillips "Mechanics of Flight"
     num = np.sqrt(np.pi*e*Ra)
@@ -14,15 +21,32 @@ def L2D_max(e,Ra,CD0,CD0_L):
 #Thrust Required as a function of lift and drag coefficients and weight
 def thrust_req_coeff(CL,CD,W):
     """ Computes the thrust required for steady level flight assuming small thrust angles given the lift and drag coefficients and weight. 
-    The units for thrust are the same as the input units used for the weight."""
+    The units for thrust are the same as the input units used for the weight.
+    
+    Args:
+        CL (float): Lift Coefficient at steady level flight
+        CD (float): Drag Coefficient under the same conditions as the lift coefficient
+        W (float): Weight of the aircraft
+    """
 
     #Equation 3.2.23 from Warren Phillips "Mechanics of Flight"
     return(CD/CL)*W
 
 #Thrust Required as a function of velocity and other aircraft parameters
 def thrust_req_arspd(rho,V,CD0,CD0_L,W,Sw,e,Ra):
-    """ Computes the thrust required for steady level flight assumeing small thrust angles given: air density, velocity, drag coefficients, weight, wing area, oswald efficiency, 
-    and aspect ratio. The units for thrust are the same as the input units used for the weight. """
+    """ Computes the thrust required for steady level flight assumeing small thrust angles. The units for thrust are 
+    the same as the input units used for the weight. 
+    
+    Args:
+        rho (float): Air density
+        V (float): Aispeed
+        CD0 (float): Drag coefficient at zero lift
+        CD0_L (float): The linear coefficient in the parabolic relation for drag coefficient as a function of the lift coefficient.      
+        W (float): Weight of the aircraft
+        Sw (float): Area of the main wing
+        e (float): Oswald efficiency (0-1)
+        Ra (float): Aspect ratio of the main wing 
+    """
 
     #Equation 3.2.25 from Warren Phillips "Mechanics of Flight"
     term1 = ((0.5*rho*V*V*CD0)/(W/Sw))
@@ -31,8 +55,16 @@ def thrust_req_arspd(rho,V,CD0,CD0_L,W,Sw,e,Ra):
 
 #Minimum thrust required for small thrust angles
 def thrust_req_min(CD0,CD0_L,e,Ra,W):
-    """ Computes the minimum thrust required for steady level flight assuming small thrust angles given: drag coefficients, oswald efficiency, aspect ratio, and weight
-    The units for thrust are the same as the input units used for the weight. """
+    """ Computes the minimum thrust required for steady level flight assuming small thrust angles. The units for thrust 
+    are the same as the input units used for the weight. 
+    
+    Args:
+        CD0 (float): Drag coefficient at zero lift
+        CD0_L (float): The linear coefficient in the parabolic relation for drag coefficient as a function of the lift coefficient.
+        e (float): Oswald efficiency (0-1)
+        Ra (float): Aspect ratio of the main wing
+        W (float): Weight of the aircraft        
+    """
 
     #Equation 3.2.26 from Warren Phillips "Mechanics of Flight"
     return ((2*np.sqrt((CD0/(np.pi*e*Ra))) + CD0_L)*W)
@@ -40,17 +72,36 @@ def thrust_req_min(CD0,CD0_L,e,Ra,W):
 #----------------------Power-------------------------
 
 #Power required as a function of thrust and aircraft params
-def power_req_basic(thrust_req,W,Sw,rho,CL):
-    """ Computes the power required for steady level flight assuming small thrust angles given: thrust required, weight, wing area, air density, and lift coefficient.
-    The output units depend on what is input for weight, wing area, air density, and thrust required. """
+def power_req_basic(Tr,W,Sw,rho,CL):
+    """ Computes the power required for steady level flight assuming small thrust angles. The output units depend on what is 
+    input for weight, wing area, air density, and thrust required. 
+    
+    Args:
+        Tr (float): Thrust required for steady level flight. Can be computed using thrust_req_coeff() or thrust_req_arspd().
+        W (float): Weight of the aircraft
+        Sw (float): Area of the main wing
+        rho (float): Air density
+        CL (float): Lift coefficient for the given thrust required
+    """
 
     #Equation 3.3.4 from Warren Phillips "Mechanics of Flight"
-    return(thrust_req*np.sqrt((2*(W/Sw))/(rho*CL)))
+    return(Tr*np.sqrt((2*(W/Sw))/(rho*CL)))
 
 #Power required as a function of airspeed and aircraft params
 def power_req_arspd(CD0,CD0_L,rho,V,W,Sw,e,Ra):
-    """ Computes the power required for steady level flight assuming small thrust angles given: drag coefficients, air density, velocity, weight, wing area, oswald efficiency,
-    and aspect ratio. The output units for power depend on what is input for the weight, velocity, density, and wing area. """
+    """ Computes the power required for steady level flight assuming small thrust angles. The output units for power depend on
+    what is input for the weight, velocity, density, and wing area. 
+    
+    Args:
+        CD0 (float): Drag coefficient at zero lift
+        CD0_L (float): The linear coefficient in the parabolic relation for drag coefficient as a function of the lift coefficient.
+        rho (float): Air density
+        V (float): Aispeed
+        W (float): Weight of the aircraft
+        Sw (float): Area of the main wing
+        e (float): Oswald efficiency (0-1)
+        Ra (float): Aspect ratio of the main wing       
+    """
 
     #Equation 3.3.7 from Warren Phillips "Mechanics of Flight"
     term1 = (CD0*rho*V**3)/(2*(W/Sw))
